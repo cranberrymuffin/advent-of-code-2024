9adf2f776c617581f9f9b5de7396f4ebe142c4b4
cranberrymuffin <aparnalovestocode@gmail.com>
2024-12-23 18:04:54 -0500

    day 22 part 2 helper functions

--- dec22.py
+++ dec22.py
@@ -18,6 +18,22 @@ def find_nth_secret_number(initial_secret_num, n):
         secret = compute_next_secret(secret)
     return secret
 
+def get_prices_for_secret_number(initial_secret_num, n):
+    prices = []
+    secret = initial_secret_num
+    for i in range(n):
+        prices.append(secret % 10)
+        secret = compute_next_secret(secret)
+    return prices
+
+def get_changes(prices):
+    changes = [None]
+    secret = initial_secret_num
+    for i in range(1, len(prices)):
+        changes.append(prices[i] - prices[i-1])
+        secret = compute_next_secret(secret)
+    return changes
+
 with open("input/dec22.txt", "r") as file:
     v1 = 0
     n = 2000
@@ -25,3 +41,5 @@ with open("input/dec22.txt", "r") as file:
         initial_secret_num = int(re.search(r'\d+', line.strip()).group())
         v1 += find_nth_secret_number(initial_secret_num, n)
     print(v1)
+
+    prices = get_prices_for_secret_number(123, 10)
