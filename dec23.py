f484e4245e172d726a6e8ec2102958b8c8d020f0
cranberrymuffin <aparnalovestocode@gmail.com>
2024-12-23 17:50:29 -0500

    dec 23 part 11

--- dec23.py
+++ dec23.py
@@ -1,12 +1,32 @@
+max = 0
+max_set = set()
+
+def BronKerbosch1(R, P, X, neighbor_set):
+    global max
+    global max_set
+    if len(P) == 0 and len(X) == 0:
+        if(len(R) > max):
+            max_set = R
+            max = len(max_set)
+
+    list_P = list(P)
+    while(len(list_P) > 0):
+        v = list_P.pop()
+        BronKerbosch1(R.union({v}), set(list_P).intersection(neighbor_set[v]), X.intersection(neighbor_set[v]), neighbor_set)
+        X = X.intersection({v})
+
 connections = {}
 
 with open("input/dec23.txt", "r") as file:
+    vertex_set = set()
     while line := file.readline():
         connection = line.strip().split("-")
         if connection[0] not in connections:
             connections[connection[0]] = set()
         if connection[1] not in connections:
             connections[connection[1]] = set()
+        vertex_set.add(connection[0])
+        vertex_set.add(connection[1])
         connections[connection[0]].add(connection[1])
         connections[connection[1]].add(connection[0])
 
@@ -25,3 +45,7 @@ with open("input/dec23.txt", "r") as file:
             v1 += 1
     print(v1)
 
+    BronKerbosch1(set(), vertex_set, set(), connections)
+    password = list(max_set)
+    password.sort()
+    print(",".join(password))
